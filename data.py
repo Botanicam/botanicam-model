88812f8f05c90794880fb1744fd8d0d616de45dd
Ignatius <112528783+Chary-x@users.noreply.github.com>
2023-08-31 20:16:48 +0100

    Update data.py

--- data.py
+++ data.py
@@ -3,7 +3,9 @@ from typing import *
 
 import torch
 import numpy as np
-from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import ToTensor
+from torch.utils.data import Subset, DataLoader
 
 from const import *
 
